ab3976f52ed49423de1ac1f1c2e02c8f515dba20
static <kmc7468@naver.com>
2024-11-08 21:31:01 +0900

    서버 초기화 코드 추가

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,50 @@
 import express from "express";
+import z from "zod";
 
 import { port, trustProxy } from "./loadenv";
+import TokenStore from "./modules/tokenStore";
 
-const app = express();
+startServer();
 
-app.set("trust proxy", trustProxy);
+async function startServer() {
+  await TokenStore.loadTokens(); // tokens.json 파일이 존재하면 불러옴
+  await TokenStore.getAccessToken(); // 기본 토큰 자동 발급
 
-// 미들웨어
-app.use(express.json());
+  await registerCommands(); // 커맨드 등록
 
-// 라우터
-// TODO
+  const app = express();
 
-app.listen(port, () => {
-  console.log(`서버가 ${port}번 포트에 실행되었습니다.`);
-});
+  app.set("trust proxy", trustProxy);
+
+  // 미들웨어
+  app.use(express.json());
+
+  // 라우터
+  app.put("/api/callback", async function (req, res) {
+    const { method, params, context } = z
+      .object({
+        method: z.string(),
+        params: z.object({}), // TODO
+        context: z.object({
+          channel: z.object({
+            id: z.string(),
+          }),
+          caller: z.object({
+            type: z.string(),
+            id: z.string(),
+          }),
+        }),
+      })
+      .parse(req.body);
+
+    // TODO
+  });
+
+  app.listen(port, () => {
+    console.log(`서버가 ${port}번 포트에 실행되었습니다.`);
+  });
+}
+
+async function registerCommands() {
+  // TODO
+}
